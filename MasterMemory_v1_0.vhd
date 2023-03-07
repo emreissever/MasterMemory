636862a8bc9494ed9e3e4e5cb204ea8b95c028f3
Emre İŞSEVER <emre.issever@outlook.com>
2023-03-07 17:06:32 +0300

    Add files via upload

---
+++ MasterMemory_v1_0.vhd
@@ -0,0 +1,249 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity MasterMemory_v1_0 is
+	generic (
+		-- Users to add parameters here
+
+		-- User parameters ends
+		C_M00_BRAM_SIZE_IN_WORDS : integer := 16384;
+
+		C_M00_START_BRAM_INDEX 		: 	std_logic_vector 	:= x"00000000";
+		-- Do not modify the parameters beyond this line
+
+
+		-- Parameters of Axi Master Bus Interface M00_AXI
+		C_M00_AXI_START_DATA_VALUE	: std_logic_vector	:= x"00000000";
+		C_M00_AXI_TARGET_SLAVE_BASE_ADDR	: std_logic_vector	:= x"40000000";
+		C_M00_AXI_ADDR_WIDTH	: integer	:= 32;
+		C_M00_AXI_DATA_WIDTH	: integer	:= 32;
+		C_M00_AXI_TRANSACTIONS_NUM	: integer	:= 4;
+
+		-- Parameters of Axi Slave Bus Interface S01_AXI
+		C_S01_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S01_AXI_ADDR_WIDTH	: integer	:= 11
+	);
+	port (
+		-- Users to add ports here
+
+		-- User ports ends
+		-- Do not modify the ports beyond this line
+
+
+		-- Ports of Axi Master Bus Interface M00_AXI
+		m00_axi_init_axi_txn	: in std_logic;
+		m00_axi_error	: out std_logic;
+		m00_axi_txn_done	: out std_logic;
+		m00_axi_aclk	: in std_logic;
+		m00_axi_aresetn	: in std_logic;
+		m00_axi_awaddr	: out std_logic_vector(C_M00_AXI_ADDR_WIDTH-1 downto 0);
+		m00_axi_awprot	: out std_logic_vector(2 downto 0);
+		m00_axi_awvalid	: out std_logic;
+		m00_axi_awready	: in std_logic;
+		m00_axi_wdata	: out std_logic_vector(C_M00_AXI_DATA_WIDTH-1 downto 0);
+		m00_axi_wstrb	: out std_logic_vector(C_M00_AXI_DATA_WIDTH/8-1 downto 0);
+		m00_axi_wvalid	: out std_logic;
+		m00_axi_wready	: in std_logic;
+		m00_axi_bresp	: in std_logic_vector(1 downto 0);
+		m00_axi_bvalid	: in std_logic;
+		m00_axi_bready	: out std_logic;
+		m00_axi_araddr	: out std_logic_vector(C_M00_AXI_ADDR_WIDTH-1 downto 0);
+		m00_axi_arprot	: out std_logic_vector(2 downto 0);
+		m00_axi_arvalid	: out std_logic;
+		m00_axi_arready	: in std_logic;
+		m00_axi_rdata	: in std_logic_vector(C_M00_AXI_DATA_WIDTH-1 downto 0);
+		m00_axi_rresp	: in std_logic_vector(1 downto 0);
+		m00_axi_rvalid	: in std_logic;
+		m00_axi_rready	: out std_logic;
+
+		-- Ports of Axi Slave Bus Interface S01_AXI
+		s01_axi_aclk	: in std_logic;
+		s01_axi_aresetn	: in std_logic;
+		s01_axi_awaddr	: in std_logic_vector(C_S01_AXI_ADDR_WIDTH-1 downto 0);
+		s01_axi_awprot	: in std_logic_vector(2 downto 0);
+		s01_axi_awvalid	: in std_logic;
+		s01_axi_awready	: out std_logic;
+		s01_axi_wdata	: in std_logic_vector(C_S01_AXI_DATA_WIDTH-1 downto 0);
+		s01_axi_wstrb	: in std_logic_vector((C_S01_AXI_DATA_WIDTH/8)-1 downto 0);
+		s01_axi_wvalid	: in std_logic;
+		s01_axi_wready	: out std_logic;
+		s01_axi_bresp	: out std_logic_vector(1 downto 0);
+		s01_axi_bvalid	: out std_logic;
+		s01_axi_bready	: in std_logic;
+		s01_axi_araddr	: in std_logic_vector(C_S01_AXI_ADDR_WIDTH-1 downto 0);
+		s01_axi_arprot	: in std_logic_vector(2 downto 0);
+		s01_axi_arvalid	: in std_logic;
+		s01_axi_arready	: out std_logic;
+		s01_axi_rdata	: out std_logic_vector(C_S01_AXI_DATA_WIDTH-1 downto 0);
+		s01_axi_rresp	: out std_logic_vector(1 downto 0);
+		s01_axi_rvalid	: out std_logic;
+		s01_axi_rready	: in std_logic
+	);
+end MasterMemory_v1_0;
+
+architecture arch_imp of MasterMemory_v1_0 is
+
+	-- Signals 
+
+	signal s_ibram_en 	  : 	std_logic;
+	signal s_ibram_addr	  : 	std_logic_vector(31 downto 0);
+	signal s_ibram_wdata  : 	std_logic_vector(C_M00_AXI_DATA_WIDTH-1 downto 0);
+
+
+	-- component declaration
+	component MasterMemory_v1_0_M00_AXI is
+		generic (
+		C_M_BRAM_SIZE_IN_WORDS  : integer := 2048;
+		C_M_START_BRAM_INDEX 		: 	std_logic_vector 	:= x"00000000";
+		C_M_START_DATA_VALUE	: std_logic_vector	:= x"00000000";
+		C_M_TARGET_SLAVE_BASE_ADDR	: std_logic_vector	:= x"00000000";
+		C_M_AXI_ADDR_WIDTH	: integer	:= 32;
+		C_M_AXI_DATA_WIDTH	: integer	:= 32;
+		C_M_TRANSACTIONS_NUM	: integer	:= 4
+		);
+		port (
+		M_IBRAM_WEN		: in std_logic ;
+		M_IBRAM_ADDR	: in std_logic_vector (31 downto 0);
+		M_TBRAM_WDATA	: in std_logic_vector (C_M_AXI_DATA_WIDTH-1 downto 0);
+		INIT_AXI_TXN	: in std_logic;
+		ERROR	: out std_logic;
+		TXN_DONE	: out std_logic;
+		M_AXI_ACLK	: in std_logic;
+		M_AXI_ARESETN	: in std_logic;
+		M_AXI_AWADDR	: out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
+		M_AXI_AWPROT	: out std_logic_vector(2 downto 0);
+		M_AXI_AWVALID	: out std_logic;
+		M_AXI_AWREADY	: in std_logic;
+		M_AXI_WDATA	: out std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
+		M_AXI_WSTRB	: out std_logic_vector(C_M_AXI_DATA_WIDTH/8-1 downto 0);
+		M_AXI_WVALID	: out std_logic;
+		M_AXI_WREADY	: in std_logic;
+		M_AXI_BRESP	: in std_logic_vector(1 downto 0);
+		M_AXI_BVALID	: in std_logic;
+		M_AXI_BREADY	: out std_logic;
+		M_AXI_ARADDR	: out std_logic_vector(C_M_AXI_ADDR_WIDTH-1 downto 0);
+		M_AXI_ARPROT	: out std_logic_vector(2 downto 0);
+		M_AXI_ARVALID	: out std_logic;
+		M_AXI_ARREADY	: in std_logic;
+		M_AXI_RDATA	: in std_logic_vector(C_M_AXI_DATA_WIDTH-1 downto 0);
+		M_AXI_RRESP	: in std_logic_vector(1 downto 0);
+		M_AXI_RVALID	: in std_logic;
+		M_AXI_RREADY	: out std_logic
+		);
+	end component MasterMemory_v1_0_M00_AXI;
+
+	component MasterMemory_v1_0_S01_AXI is
+		generic (
+		C_S_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S_AXI_ADDR_WIDTH	: integer	:= 4
+		);
+		port (
+		S_IBRAM_WEN	: out std_logic;
+		S_IBRAM_ADDR 	: out std_logic_vector (31 downto 0);
+		S_IBRAM_WDATA : out std_logic_vector (C_M00_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_ACLK	: in std_logic;
+		S_AXI_ARESETN	: in std_logic;
+		S_AXI_AWADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_AWPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_AWVALID	: in std_logic;
+		S_AXI_AWREADY	: out std_logic;
+		S_AXI_WDATA	: in std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_WSTRB	: in std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+		S_AXI_WVALID	: in std_logic;
+		S_AXI_WREADY	: out std_logic;
+		S_AXI_BRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_BVALID	: out std_logic;
+		S_AXI_BREADY	: in std_logic;
+		S_AXI_ARADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_ARPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_ARVALID	: in std_logic;
+		S_AXI_ARREADY	: out std_logic;
+		S_AXI_RDATA	: out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_RRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_RVALID	: out std_logic;
+		S_AXI_RREADY	: in std_logic
+		);
+	end component MasterMemory_v1_0_S01_AXI;
+
+begin
+
+-- Instantiation of Axi Bus Interface M00_AXI
+MasterMemory_v1_0_M00_AXI_inst : MasterMemory_v1_0_M00_AXI
+	generic map (
+		C_M_BRAM_SIZE_IN_WORDS  => C_M00_BRAM_SIZE_IN_WORDS,
+		C_M_START_BRAM_INDEX 	=> C_M00_START_BRAM_INDEX,
+		C_M_START_DATA_VALUE	=> C_M00_AXI_START_DATA_VALUE,
+		C_M_TARGET_SLAVE_BASE_ADDR	=> C_M00_AXI_TARGET_SLAVE_BASE_ADDR,
+		C_M_AXI_ADDR_WIDTH	=> C_M00_AXI_ADDR_WIDTH,
+		C_M_AXI_DATA_WIDTH	=> C_M00_AXI_DATA_WIDTH,
+		C_M_TRANSACTIONS_NUM	=> C_M00_AXI_TRANSACTIONS_NUM
+	)
+	port map (
+		M_IBRAM_WEN		=> s_ibram_en,
+		M_IBRAM_ADDR  	=> s_ibram_addr, 
+		M_TBRAM_WDATA 	=> s_ibram_wdata,
+		INIT_AXI_TXN	=> m00_axi_init_axi_txn,
+		ERROR	=> m00_axi_error,
+		TXN_DONE	=> m00_axi_txn_done,
+		M_AXI_ACLK	=> m00_axi_aclk,
+		M_AXI_ARESETN	=> m00_axi_aresetn,
+		M_AXI_AWADDR	=> m00_axi_awaddr,
+		M_AXI_AWPROT	=> m00_axi_awprot,
+		M_AXI_AWVALID	=> m00_axi_awvalid,
+		M_AXI_AWREADY	=> m00_axi_awready,
+		M_AXI_WDATA	=> m00_axi_wdata,
+		M_AXI_WSTRB	=> m00_axi_wstrb,
+		M_AXI_WVALID	=> m00_axi_wvalid,
+		M_AXI_WREADY	=> m00_axi_wready,
+		M_AXI_BRESP	=> m00_axi_bresp,
+		M_AXI_BVALID	=> m00_axi_bvalid,
+		M_AXI_BREADY	=> m00_axi_bready,
+		M_AXI_ARADDR	=> m00_axi_araddr,
+		M_AXI_ARPROT	=> m00_axi_arprot,
+		M_AXI_ARVALID	=> m00_axi_arvalid,
+		M_AXI_ARREADY	=> m00_axi_arready,
+		M_AXI_RDATA	=> m00_axi_rdata,
+		M_AXI_RRESP	=> m00_axi_rresp,
+		M_AXI_RVALID	=> m00_axi_rvalid,
+		M_AXI_RREADY	=> m00_axi_rready
+	);
+
+-- Instantiation of Axi Bus Interface S01_AXI
+MasterMemory_v1_0_S01_AXI_inst : MasterMemory_v1_0_S01_AXI
+	generic map (
+		C_S_AXI_DATA_WIDTH	=> C_S01_AXI_DATA_WIDTH,
+		C_S_AXI_ADDR_WIDTH	=> C_S01_AXI_ADDR_WIDTH
+	)
+	port map (
+		S_IBRAM_WEN	 	=> s_ibram_en,	
+		S_IBRAM_ADDR 	=> s_ibram_addr, 
+		S_IBRAM_WDATA	=> s_ibram_wdata,
+		S_AXI_ACLK	    => s01_axi_aclk,
+		S_AXI_ARESETN	=> s01_axi_aresetn,
+		S_AXI_AWADDR	=> s01_axi_awaddr,
+		S_AXI_AWPROT	=> s01_axi_awprot,
+		S_AXI_AWVALID	=> s01_axi_awvalid,
+		S_AXI_AWREADY	=> s01_axi_awready,
+		S_AXI_WDATA	=> s01_axi_wdata,
+		S_AXI_WSTRB	=> s01_axi_wstrb,
+		S_AXI_WVALID	=> s01_axi_wvalid,
+		S_AXI_WREADY	=> s01_axi_wready,
+		S_AXI_BRESP	=> s01_axi_bresp,
+		S_AXI_BVALID	=> s01_axi_bvalid,
+		S_AXI_BREADY	=> s01_axi_bready,
+		S_AXI_ARADDR	=> s01_axi_araddr,
+		S_AXI_ARPROT	=> s01_axi_arprot,
+		S_AXI_ARVALID	=> s01_axi_arvalid,
+		S_AXI_ARREADY	=> s01_axi_arready,
+		S_AXI_RDATA	=> s01_axi_rdata,
+		S_AXI_RRESP	=> s01_axi_rresp,
+		S_AXI_RVALID	=> s01_axi_rvalid,
+		S_AXI_RREADY	=> s01_axi_rready
+	);
+
+	-- Add user logic here
+
+	-- User logic ends
+
+end arch_imp;
